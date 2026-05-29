ac025d28b77903e6421ebb87017ab53581853155
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-29 21:43:36 +0200

    Added the main entry point for the application

---
+++ quitchen_backend/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+
+async function bootstrap() {
+    const app =
+        await NestFactory.create(AppModule)
+
+    app.setGlobalPrefix('api')
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            forbidNonWhitelisted: true
+        })
+    )
+
+    app.enableCors({
+        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+        credentials: true,
+    })
+
+    const port =
+        process.env.PORT || 4500
+
+    await app.listen(port);
+    console.log(`Quitchen API running on: http://localhost:${port}/api`)
+}
+
+bootstrap()
