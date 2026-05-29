76a8f9e4f3717be58d2f2369c680fe799a83fe6a
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-29 21:06:50 +0200

    Created a databse Connections module to be used accross the application to initialized neonm postegres database connections

--- quitchen_backend/src/db/db.module.ts
+++ quitchen_backend/src/db/db.module.ts
@@ -1,1 +1,29 @@
-import { Global, Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema';
+
+
+export const DB = Symbol('DB');
+
+@Global() //This makes the module available accross the application
+@Module({
+    providers: [
+        {
+            provide: DB,
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                const databaseUrl = config.get<string>('DATABASE_URL');
+                if (!databaseUrl) {
+                    throw new Error('DATABASE_URL environment variable is not set');
+                }
+                const sql = neon(databaseUrl);
+                return drizzle(sql, { schema });
+            },
+        },
+    ],
+    exports: [DB],
+})
+
+export class dbModule { };
