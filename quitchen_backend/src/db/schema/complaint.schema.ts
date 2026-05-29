e547aee16e73368f8c3e47c2ee59216bccefb40d
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-29 15:39:52 +0200

    Maade some final touchings to the schemas and now moving on to drizzle config

---
+++ quitchen_backend/src/db/schema/complaint.schema.ts
@@ -0,0 +1,19 @@
+// This Schema will serve as to define restaurant complaints
+
+import { pgTable, text, uuid, timestamp } from "drizzle-orm/pg-core";
+import { restaurant } from "./restaurant.schema";
+
+export const complaint = pgTable('complaints', {
+    id: uuid('id').primaryKey().notNull().defaultRandom().$defaultFn(() => crypto.randomUUID()),
+    restaurantId: text('restaurant_id')
+        .notNull()
+        .references(() => restaurant.id, { onDelete: 'cascade' }),
+    subject: text('subject').notNull(),
+    description: text('description').notNull(),
+    status: text('status', {
+        enum: ['open', 'under_review', 'resolved', 'closed']
+    }).notNull().default('open'),
+    adminResponse: text('admin_response'),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
---
+++ quitchen_backend/src/db/schema/index.ts
@@ -0,0 +1,6 @@
+export * from './auth.schema';
+export * from './complaint.schema';
+export * from './menu.schema';
+export * from './order.schema';
+export * from './reservation.schema';
+export * from './restaurant.schema'
