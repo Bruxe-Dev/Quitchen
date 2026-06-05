0d8111a6672acd5261ebc84044f69837728a1c6f
Bruxe-Dev <mnibeza23@gmail.com>
2026-06-05 10:19:54 +0200

    Created a Custom role guard to be used to check the user againt the roles to avoid unauthorized access

---
+++ quitchen_backend/src/common/guards/role.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, Injectable, ForbiddenException, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+
+@Injectable()
+export class RouteGuard implements CanActivate {
+    constructor(private reflector: Reflector) { }
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.get<string[]>(
+            'roles',
+            context.getHandler(),
+        );
+
+        if (!requiredRoles) {
+            return true
+        }
+
+        const request = context.switchToHttp().getRequest()
+        const user = request.user;
+
+        if (!user) {
+            throw new ForbiddenException("User not Found")
+        }
+        if (!requiredRoles.includes(user.role)) {
+            throw new ForbiddenException("Unauthorized")
+        }
+
+        return true
+    }
+}
